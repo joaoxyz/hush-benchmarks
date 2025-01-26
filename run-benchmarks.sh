18282dc7af5ecfd803dca07b83bb6a495aab2c91
joaoxyz <jpreis808@gmail.com>
2025-01-25 22:43:32 -0300

    Add benchmark run script

---
+++ run-benchmarks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+programs=(binary-trees fannkuch-redux fasta regex-redux spectral-norm)
+
+if [[ "$1" == "--large-args" ]]; then
+    args=(21 12 25000000 "< rr-input5000000.txt" 5500)
+elif [[ "$#" -eq 0 ]]; then
+    args=(10 7 1000 "< rr-input1000.txt" 100)
+else
+    echo "Invalid arguments"
+    exit 1
+fi
+
+mkdir -p benchmarks/{json,md}
+
+for i in {0..4}; do
+    hyperfine -r 50 -w 10 "hush src/hush/${programs[i]}.hsh ${args[i]}" "python src/python/${programs[i]}.py ${args[i]}" --export-json benchmarks/json/${programs[i]}.json --export-markdown benchmarks/md/${programs[i]}.md
+done
