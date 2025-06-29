65599933e704d8e3cb06eedc42ec19d42b3676a0
joaoxyz <jpreis808@gmail.com>
2025-06-29 13:05:11 -0300

    Fix spectral-norm and change python implementation to match

--- src/python/spectral-norm.py
+++ src/python/spectral-norm.py
@@ -1,54 +1,38 @@
 # The Computer Language Benchmarks Game
-# https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+#   https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
 #
-# Contributed by Sebastien Loisel
-# Fixed by Isaac Gouy
-# Sped up by Josh Goldfoot
-# Dirtily sped up by Simon Descarpentries
-# Used list comprehension by Vadim Zelenin
-# 2to3
+#   Naive transliteration from Sebastien Loisel's C program
+#   contributed by Isaac Gouy
+#
 
 from math import sqrt
-from sys import argv
+import sys
 
 def eval_A(i, j):
-    ij = i + j
-    return 1.0 / (ij * (ij + 1) / 2 + i + 1)
+  return 1.0/((i+j)*(i+j+1)/2+i+1)
 
-def eval_A_times_u(u):
-    local_eval_A = eval_A
+def eval_A_times_u(N, u, Au):
+  for i in range(N):
+    Au[i]=0
+    for j in range(N): Au[i]+=eval_A(i,j)*u[j]
 
-    return [
-        sum([local_eval_A(i, j) * u_j for j, u_j in enumerate(u)])
-        for i in range(len(u))
-    ]
+def eval_At_times_u(N, u, Au):
+  for i in range(N):
+    Au[i]=0
+    for j in range(N): Au[i]+=eval_A(j,i)*u[j]
 
-def eval_At_times_u(u):
-    local_eval_A = eval_A
+def eval_AtA_times_u(N, u, AtAu):
+  v=[0]*N; eval_A_times_u(N,u,v); eval_At_times_u(N,v,AtAu)
 
-    return [
-        sum([local_eval_A(j, i) * u_j for j, u_j in enumerate(u)])
-        for i in range(len(u))
-    ]
+def main(n):
+  u=[1]*n
+  v=[0]*n
+  for i in range(10):
+    eval_AtA_times_u(n,u,v)
+    eval_AtA_times_u(n,v,u)
+  vBv=vv=0
+  for i in range(n): vBv+=u[i]*v[i]; vv+=v[i]*v[i]
+  print("%.9f" % sqrt(vBv/vv))
 
-def eval_AtA_times_u(u):
-    return eval_At_times_u(eval_A_times_u(u))
-
-def main():
-    n = int(argv[1])
-    u = [1] * n
-    local_eval_AtA_times_u = eval_AtA_times_u
-
-    for dummy in range(10):
-        v = local_eval_AtA_times_u(u)
-        u = local_eval_AtA_times_u(v)
-
-    vBv = vv = 0
-
-    for ue, ve in zip(u, v):
-        vBv += ue * ve
-        vv += ve * ve
-
-    print("%0.9f" % (sqrt(vBv / vv)))
-
-main()
+if __name__ == '__main__':
+  main( int(sys.argv[1]) if len(sys.argv) > 1 else 100 )  
